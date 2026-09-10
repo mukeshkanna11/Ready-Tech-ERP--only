b0178cb127190c965f2a0d3ad55739633da903c3
Mukeshkanna M <mukeshmernova@gmail.com>
2026-09-10 12:48:57 +0530

    cors change

--- Backend/src/config/cors.js
+++ Backend/src/config/cors.js
@@ -1,10 +1,39 @@
-const env = require('./env');
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:3000",
+  "http://localhost:8081",
+  "http://127.0.0.1:5173",
+  "http://127.0.0.1:3000",
+  "https://readytech-erp.vercel.app",
+];
 
 const corsOptions = {
-  origin: env.CLIENT_URL,
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+
+  methods: [
+    "GET",
+    "POST",
+    "PUT",
+    "PATCH",
+    "DELETE",
+    "OPTIONS",
+  ],
+
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+  ],
 };
 
 module.exports = corsOptions;
